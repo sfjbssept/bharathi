5f9cb0c8c261745770d1c73084b4a996924e2522
sfjbssept <sfjbssept@gmal.com>
2022-09-09 07:05:09 +0000

    Adding project

---
+++ ProjectManagementSYstem/src/main/java/com/product/ProjectManagementSYstemApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSYstemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementSYstemApplication.class, args);
+	}
+
+}
