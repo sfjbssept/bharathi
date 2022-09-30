02d1093bf0a507f4d47f0df35cc95d675473e626
sfjbssept <sfjbssept@gmal.com>
2022-09-30 06:26:59 +0000

    Adding todays files v1.1

--- Kafka-Project/kafka-publish/src/main/java/KafkaPublishApplication.java
+++ Kafka-Project/kafka-publish/src/main/java/KafkaPublishApplication.java
@@ -1,4 +1,4 @@
-package com.publish;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +7,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.publish.entity.User;
 
 @RestController
 @SpringBootApplication
@@ -24,14 +22,6 @@ public class KafkaPublishApplication {
 	public String publishmessage(@PathVariable String name) {
 		template.send(topic,"Hi "+name+" this is to inform that the message been sent fron Kafka Publisher");
 		return "Data been published";
-		
-	}	
-	
-	@GetMapping("/publishjson")
-	public String publishmessage() {
-		User user = new User(101, "Bharathi",new String[] {"Yelhanka","Bangalore"});
-		template.send(topic,user);
-		return "JSON Data been published";
 		
 	}
 	public static void main(String[] args) {
